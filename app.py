87cafe5705293fea56c5bb28098482b89cda22ce
Tejas Satish Navalkhe <navalkhetejas18@gmail.com>
2024-08-11 14:44:51 +0100

    Sum of Total Revenue code added

--- app.py
+++ app.py
@@ -3,23 +3,6 @@ from utils.model import *
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import warnings
-from scipy import stats
-from matplotlib.ticker import FuncFormatter
-from category_encoders import BinaryEncoder, OneHotEncoder
-from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-import joblib
 
 # Data location
 DATA_PATH = "../data/"
@@ -27,6 +10,8 @@ DATA_PATH = "../data/"
 ENCODERS_PATH = "encoders/"
 # Model location
 MODEL_PATH = "models/"
+
+
 
 app = Flask(__name__)
 
@@ -54,7 +39,8 @@ def index():
         predictions_df = apply_dynamic_pricing_strategy(predictions_df, demand_factor_value)
 
         # adjust final prices
-        predictions_df = predictions_df.apply(adjust_prices, axis=1)
+        grouped_averages = get_average_rates(df, location)
+        predictions_df = predictions_df.apply(lambda row: adjust_prices(row, grouped_averages), axis=1)
         predictions_df.rename(columns={'hourly_rate': 'current_hourly_rate', 
                                        'daily_rate': 'current_daily_rate', 'final_hourly_rate': 'adjusted_hourly_rate',
                                        'final_daily_rate': 'adjusted_daily_rate'}, inplace=True)
@@ -81,6 +67,24 @@ def index():
                 'actual_revenue': row['actual_revenue'],
                 'profitability': row['profitability']
             }
+
+        # Initialize sums
+        total_adjusted_revenue = 0
+        total_actual_revenue = 0
+        total_profitability = 0
+
+        # Iterate through the dictionary and sum up the values
+        for key, value in profitability.items():
+            total_adjusted_revenue += value['adjusted_revenue']
+            total_actual_revenue += value['actual_revenue']
+            total_profitability += value['profitability']
+
+        # Add the new key with summed values
+        profitability['Z'] = {
+            'adjusted_revenue': total_adjusted_revenue,
+            'actual_revenue': total_actual_revenue,
+            'profitability': total_profitability
+        }
 
         return jsonify(peakHours=peak_hours, predictions=predictions, profitability=profitability)
     return render_template('index.html', locations=locations)
